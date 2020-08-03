c87b1ecd856e15b5c41b5493412dec0d13377758
GitHub Action <action@github.com>
2020-08-03 21:48:53 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/0leksandr/intellijcontextstash/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.0leksandr.intellijcontextstash.services
+
+import com.github.0leksandr.intellijcontextstash.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/0leksandr/intellijcontextstash/services/MyProjectService.kt
+++ src/main/kotlin/com/github/0leksandr/intellijcontextstash/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.0leksandr.intellijcontextstash.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.0leksandr.intellijcontextstash.MyBundle
 
 class MyProjectService(project: Project) {
 
